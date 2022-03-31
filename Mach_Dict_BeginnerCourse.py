8e546eae89d50385aa140beb6fb06eb74e2a49fc
JvMapote <jayveenmapote@gmail.com>
2022-04-01 03:56:24 +0800

    Update Mach_Dict_BeginnerCourse.py

--- Mach_Dict_BeginnerCourse.py
+++ Mach_Dict_BeginnerCourse.py
@@ -54,3 +54,17 @@ def functions():
         print("Hello, " + name + "!")
 
     hello_function("jv")
+
+def input_Function():
+    #using input
+
+    name = input("Enter your name: ")
+    print("Hi " + name + "!")
+
+    def addNum(num1, num2):
+        total = num1 + num2
+        return(total)
+
+    num1 = int(input("Please enter your first number: "))
+    num2 = int(input("Please enter your second number: "))
+    print("Total: ", addNum(num1, num2))
