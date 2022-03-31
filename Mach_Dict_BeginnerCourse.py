38c7bfaea61eb95667ecd030fba30e465bd72ff1
JvMapote <jayveenmapote@gmail.com>
2022-04-01 03:38:07 +0800

    Update Mach_Dict_BeginnerCourse.py

--- Mach_Dict_BeginnerCourse.py
+++ Mach_Dict_BeginnerCourse.py
@@ -1,2 +1,38 @@
-#Hello world
-print("Hello world")
+def HelloWorld():
+    #Hello world
+    print("Hello world")
+    print("I love cakes")
+
+def Variables():
+    #Variables
+    x = 7
+    y = "cake"
+
+    print(x)
+    print(y)
+
+    #String concatenation
+
+    favorite_food = "pizza"
+    friend_name = "tina"
+    favorite_drink = "soda"
+
+    print("I like " + favorite_food + ".")
+    print("I like eating " + favorite_food + " with my friend, " + friend_name + ".")
+    print("We like to drink " + favorite_drink + ".")
+
+def syntax_error():
+    #Syntax and error
+    favorite_food = set ("pizza")
+    if favorite_food == "pizza":
+        print("I like pizza")
+
+def challenges():
+    #challenge 1
+    string = "this is a string."
+    print(string)
+
+    #challenge 2
+    print("I could use a cup of coffee.")
+
+    #challenge 3
